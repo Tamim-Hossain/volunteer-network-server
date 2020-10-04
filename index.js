8e5a28b58540754878a182b59be259926196dd7f
Tamim <tamim-hossain>
2020-10-04 21:14:07 +0600

    almost done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q43xx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
@@ -12,14 +13,39 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 client.connect(err => {
     const eventsCollection = client.db("volunteerdb").collection("events");
+    const registersCollection = client.db("volunteerdb").collection("registers");
+
     app.get('/events', (req, res) => {
         eventsCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
             })
     })
+
+    app.post('/register', (req, res) => {
+        const register = req.body;
+        registersCollection.insertOne(register)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/register', (req, res) => {
+        registersCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.delete('/delete/:id', (req, res) => {
+        registersCollection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then((result) => {
+                res.send(result.deletedCount > 0);
+            });
+    });
 });
 
 app.listen(port)
